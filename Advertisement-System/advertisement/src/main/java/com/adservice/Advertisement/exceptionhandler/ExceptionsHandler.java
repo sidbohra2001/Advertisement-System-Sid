a1378802e75357d57a9dc1605daa31ee0523f36b
sidbohra <sidmessiah10@gmail.com>
2023-10-01 18:40:21 +0530

    tweaks

--- Advertisement-System/advertisement/src/main/java/com/adservice/Advertisement/exceptionhandler/ExceptionsHandler.java
+++ Advertisement-System/advertisement/src/main/java/com/adservice/Advertisement/exceptionhandler/ExceptionsHandler.java
@@ -1,34 +1,46 @@
 package com.adservice.Advertisement.exceptionhandler;// package com.inventory.Order.exceptionhandler;
 
 
-import com.adservice.Advertisement.exceptions.*;
+import com.adservice.Advertisement.exceptions.InvalidIdException;
+import com.adservice.Advertisement.exceptions.MismatchException;
+import com.adservice.Advertisement.exceptions.NoDataException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestClientResponseException;
 
 @ControllerAdvice
 public class ExceptionsHandler {
 
     @ExceptionHandler(InvalidIdException.class)
-    public ResponseEntity<ExceptionFormat> handleInvalidIdException(InvalidIdException e){
+    public ResponseEntity<ExceptionFormat> handleInvalidIdException(InvalidIdException e) {
         return new ResponseEntity<>(ExceptionFormat.builder().statusCode(HttpStatus.BAD_REQUEST.name()).message(e.getMessage()).build(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoDataException.class)
-    public ResponseEntity<ExceptionFormat> handleNoDataException(NoDataException e){
+    public ResponseEntity<ExceptionFormat> handleNoDataException(NoDataException e) {
         return new ResponseEntity<>(ExceptionFormat.builder().statusCode(HttpStatus.NOT_FOUND.name()).message(e.getMessage()).build(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MismatchException.class)
-    public ResponseEntity<ExceptionFormat> handleMismatchException(MismatchException e){
+    public ResponseEntity<ExceptionFormat> handleMismatchException(MismatchException e) {
+        return new ResponseEntity<>(ExceptionFormat.builder().statusCode(HttpStatus.BAD_REQUEST.name()).message(e.getMessage()).build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionFormat> handleMismatchException(MethodArgumentNotValidException e) {
         return new ResponseEntity<>(ExceptionFormat.builder().statusCode(HttpStatus.BAD_REQUEST.name()).message(e.getMessage()).build(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RestClientResponseException.class)
-    public ResponseEntity<ExceptionFormat> handleRestClientException(RestClientResponseException e){
+    public ResponseEntity<ExceptionFormat> handleRestClientException(RestClientResponseException e) {
         return new ResponseEntity<>(ExceptionFormat.builder().statusCode(e.getResponseBodyAs(ExceptionFormat.class).getStatusCode()).message(e.getResponseBodyAs(ExceptionFormat.class).getMessage()).build(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionFormat> handleException(Exception e) {
+        return new ResponseEntity<>(ExceptionFormat.builder().statusCode(HttpStatus.BAD_REQUEST.name()).message(e.getMessage()).build(), HttpStatus.BAD_REQUEST);
     }
 }
