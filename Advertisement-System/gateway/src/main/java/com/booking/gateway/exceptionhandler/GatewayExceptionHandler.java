64e33ffb705be34bb35b1365b0ddcd7068467fb5
sidbohra <sidmessiah10@gmail.com>
2023-10-01 17:54:01 +0530

    Tweaks

--- Advertisement-System/gateway/src/main/java/com/booking/gateway/exceptionhandler/GatewayExceptionHandler.java
+++ Advertisement-System/gateway/src/main/java/com/booking/gateway/exceptionhandler/GatewayExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.booking.gateway.exceptionhandler;
 
 import com.booking.gateway.exceptions.UnauthorizedUserException;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
@@ -15,6 +16,12 @@ import reactor.core.publisher.Mono;
 public class GatewayExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UnauthorizedUserException.class)
     public Mono<ServerResponse> handleIllegalState(ServerWebExchange exchange, UnauthorizedUserException exc) {
+        exchange.getAttributes().putIfAbsent(ErrorAttributes.ERROR_ATTRIBUTE, exc);
+        return ServerResponse.from(ErrorResponse.builder(exc, HttpStatus.FORBIDDEN,exc.getMessage()).build());
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public Mono<ServerResponse> handleExpiredTokenState(ServerWebExchange exchange, ExpiredJwtException exc) {
         exchange.getAttributes().putIfAbsent(ErrorAttributes.ERROR_ATTRIBUTE, exc);
         return ServerResponse.from(ErrorResponse.builder(exc, HttpStatus.FORBIDDEN,exc.getMessage()).build());
     }
